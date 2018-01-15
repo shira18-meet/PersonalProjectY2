91382f75dbfb8f6112263c9896ae28f42c34bd40
shira18-meet <shirastruminski@gmail.com>
2018-01-15 19:39:57 +0200

    first

---
+++ home.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/feed')
+def feed():
+	return render_template('feed.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/addpost')
+def addpost():
+	return render_template('addpost.html')
