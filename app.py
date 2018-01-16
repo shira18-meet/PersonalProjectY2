2585fe8039afd80235a88245e30b6200b9d2e642
shira18-meet <shirastruminski@gmail.com>
2018-01-16 18:24:41 +0200

    hopefully working ??

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-from flask import Flask
+
 app = Flask(__name__)
 from flask import render_template
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
