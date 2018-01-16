5783e260d2fcfe31df1169d4ebd009e24c737c22
shira18-meet <shirastruminski@gmail.com>
2018-01-16 17:42:42 +0200

    d

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+
 from flask import Flask
 app = Flask(__name__)
 from flask import render_template
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 @app.route('/')
 @app.route('/feed')
@@ -16,3 +19,19 @@ def about():
 def addpost():
 	return render_template('addpost.html')
 
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title=db.Column(db.String(80))
+	city = db.Column(db.String(50))
+	text = db.Column(db.String(300))
+
+
+	def __init__(self,title,city,text):
+		self.title=title
+		self.city=city
+		self.text=text
+
+	def __repr__(self):
+		return '<Title %r>' % self.title 
+	
